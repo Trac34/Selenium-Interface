3cbabd6b1e1fbe0ce0820ca047e847043910c0f4
zer0 <Z@Kakashi.local>
2021-02-27 01:25:17 -0500

    Browser Upgrade 0.3

--- Browser.py
+++ Browser.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as bs # External Dependency
 from os import getpid
+from os import getcwd
 from psutil import process_iter # External dependency 
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
@@ -9,6 +10,7 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from time import sleep
+import threading # Another External dependency. Just using a thread to for a polling function
 
 
 ## How to maintain a list of bug bounty URLs ?
@@ -37,19 +39,29 @@ So the class does not prevent the use of webdriver functions directly, but rathe
 	Not to mention that anything with a handle to the class can acess all data members directly...
 	Most of this code is based on lessons found here : https://www.geeksforgeeks.org/selenium-python-tutorial/
 	"""
-	def __init__(self, driver_path, headless=True):
+	def __init__(self, driver_path, headless=True, proxy=""):
 		try:
 			assert(type(driver_path)) == str ## Sanity-Check path is a string
 			self.options = Options()
 			self.options.headless = headless
-			self.pid = getpid()		# Useful | Not Useful ? Maybe. 
+			self.pid = getpid()		# Useful | Not Useful ? Maybe.
+			if proxy != "": # proxy defined as "<HOST:PORT>"
+				webdriver.DesiredCapabilities.FIREFOX['proxy'] = {
+					"httpProxy": proxy,
+					"ftpProxy": proxy,
+					"sslProxy": proxy,
+					"proxyType": "MANUAL",
+				}
 			self.driver = webdriver.Firefox(options=self.options, executable_path=driver_path)
 			self.rootWindowHandle = self.driver.current_window_handle # The handle to the whole browser frame / window
 			#If there is a pop-up, we need to switch window handles, but need to be able to go back to the root
+			self.handles = []
 			self.expectedChildren = "geckodriver firefox-bin".split()
 			self.pids = [] # Instantiating webdriver is a blocking call. Children should be running.
 			self.setChildPIDs()
 			self.bsource = bs    # create BeautifulSoup Object to set type
+			self.windowChecker = threading.Thread(target=self.startWindowCheck, name="windowChecker") # If another thread has to be used, switch to array of threads as thread-pool
+			self.windowChecker.start()
 		except Exception as e:
 			print("[!!] Unable to initialize Browser Class [!!]\n\n{}".format(e))
 			exit(1)
@@ -58,7 +70,7 @@ So the class does not prevent the use of webdriver functions directly, but rathe
 		print(f"[*] Please call {bcolors.OKCYAN}Browser.usage(){bcolors.ENDC} for help. Otherwise dir(Browser) and help(Browser) will also work.\n")
 		print(f"{bcolors.HEADER}[+]{bcolors.ENDC} Current Process ID [{bcolors.HEADER} %d {bcolors.ENDC}]" % self.pid)
 		for pid in self.pids:
-			print(f"\t\t{bcolors.OKGREEN}[+]{bcolors.ENDC} Child Process ID [ {bcolors.OKGREEN} %d {bcolors.ENDC} ]" % pid)
+			print(f"\t\t\t{bcolors.OKGREEN}[+]{bcolors.ENDC} Child Process ID [ {bcolors.OKGREEN} %d {bcolors.ENDC} ]" % pid)
 
 	def usage(self): ## TODO: Need to update once class is finalized
 		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.get( url ){bcolors.ENDC} will move the browser to the requested page")
@@ -67,14 +79,26 @@ So the class does not prevent the use of webdriver functions directly, but rathe
 		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.back(){bcolors.ENDC} will move the browser back one page")
 		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.forward(){bcolors.ENDC} will move the browser forward one cached page")
 		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.refresh(){bcolors.ENDC} will move the browser forward one cached page")
+		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.multipleWindows(){bcolors.ENDC} will return boolean True if multiple windows are open.")
+		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.cycleWindow( handle=None ){bcolors.ENDC} will switch windows to handle if passed, otherwise it will switch to first handle it finds.")
+		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.movetoElement( element ){bcolors.ENDC} will move cursor to given element.")
+		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.moveOffset( x, y ){bcolors.ENDC} will move cursor to given x | y positions relative to current position, i.e. new_x = current_x + x")
 		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.find_tags( query ){bcolors.ENDC} and {bcolors.BOLD}{bcolors.OKCYAN}Browser.find_tag( query ){bcolors.ENDC} will search the DOM for all matched ids or the first matched id - {bcolors.OKGREEN}returns Selenium Object{bcolors.ENDC}")
 		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.find_ids( query ){bcolors.ENDC} and {bcolors.BOLD}{bcolors.OKCYAN}Browser.find_id( query ){bcolors.ENDC} will search the DOM for all matched ids or the first matched id - {bcolors.OKGREEN}returns Selenium Object{bcolors.ENDC}")
 		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.find_names( query ){bcolors.ENDC} and {bcolors.BOLD}{bcolors.OKCYAN}Browser.find_name( query ){bcolors.ENDC} will search the DOM for all matched names or the first matched name -{bcolors.OKGREEN}returns Selenium Object{bcolors.ENDC}")
 		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.find_xpaths( query ){bcolors.ENDC} and {bcolors.BOLD}{bcolors.OKCYAN}Browser.find_xpath( query ){bcolors.ENDC} will search the DOM for all matched xpaths or the first matched xpath - {bcolors.OKGREEN}returns Selenium Object{bcolors.ENDC}")
 		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.click( element ){bcolors.ENDC} will click on the element passed to it\n\te.g. Browser.click( Browser.find_id('search_bar') )")
+		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.contextClick( element ){bcolors.ENDC} will right-click on the element passed to it\n\te.g. Browser.contextClick( Browser.find_id('search_bar') )")
+		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.clickHold( element ){bcolors.ENDC} will click and hold on the element passed to it.")
+		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.doubleClick( element ){bcolors.ENDC} will double-click on the element passed to it.")
+		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.switchFrame( element ){bcolors.ENDC} will switch context to given iFrame element.")
+		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.defaultFrame( element ){bcolors.ENDC} will switch context to the root frame.")
 		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.send_keys( element, query ){bcolors.ENDC} will send the query string to the given element.")
 		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.scrollBottom(){bcolors.ENDC} executes a javascript line to scroll to the bottom of the page. Useful for 'infinite' dynamically loading pages")
+		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.scrollTop(){bcolors.ENDC} executes a javascript line to scroll to the Top of the current page.")
+		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.b64sreenshot( path=getcwd(), name='screenshot.png', element=None ){bcolors.ENDC} creates a base64 text file under [path] with [name] or take picture of given [element].")
 		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.viewSource(){bcolors.ENDC} returns the current page's source code.")
+		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.script( script, args=None ){bcolors.ENDC} executes passed string as javascript in context of current page with [args] to the script as the second argument.")
 		print(f"\n{bcolors.BOLD}{bcolors.OKCYAN}Browser.getActionObject(){bcolors.ENDC} returns a selenium ActionChain Object so that complex actions can be performed on the DOM.")
 		print("Example : action = Browser.getActionObject()")
 		print("\taction.key_down(Keys.CONTROL).send_keys('F').key_up(Keys.CONTROL).perform() # CTRL-F # Brings up search bar")
@@ -108,6 +132,10 @@ So the class does not prevent the use of webdriver functions directly, but rathe
 		"""Refresh the current page"""
 		self.driver.refresh()
 
+	def multipleWindows(self):
+		""" False if only one handle, otherwise True. Polled using thread"""
+		return False if (len(self.driver.window_handles) == 1) else True
+
 	def cycleWindow(self, h=None):
 		""" Gather Window handles, check passed handle against discovered ( if passed )\
 			switch to window if handle is matched otherwise \
@@ -118,18 +146,22 @@ So the class does not prevent the use of webdriver functions directly, but rathe
 		for handle in self.driver.window_handles:
 			if handle != self.rootWindowHandle:
 					handles.append(handle)
+		self.handles = handles
 		if h is not None:
 			for handle in handles:
 				if handle == h:
 					self.driver.switch_to_window( handle )
+					return 0
 
-		if len(handles) > 1:
+		if len(handles) >= 1:
 			print(f"{bcolors.WARNING}[*] More than 2 windows open{bcolors.ENDC}")
 			for h in handles:
-				print("Handle: {}".format(h))
+				print("\t[+] Handle: {}".format(h))
+			print("Switching to handle: {}".format(handles[0]))
 		
 		window = handles[0] # Hardcoded default first in list
 		self.driver.switch_to_window( window )
+		return 0
 	## END CycleWindow ##
 
 	
@@ -223,6 +255,11 @@ So the class does not prevent the use of webdriver functions directly, but rathe
 		except Exception as e:
 			print("Unable to send_keys to element\n{}".format(e))
 			return -1
+
+	def scrollTop(self):
+		""" Execute javascript command to scroll to bottom of the page """
+		self.driver.execute_script("window.scrollTop(0)")
+
 
 	def scrollBottom(self):
 		""" Execute javascript command to scroll to bottom of the page """
@@ -383,6 +420,20 @@ So the class does not prevent the use of webdriver functions directly, but rathe
 		return ActionChains(self.driver)
 
 
+	def b64screenshot(self, path=getcwd(), name="screenshot.png", element=None):
+		p = path +"/"+name
+		if element is not None:
+			try:
+				assert(type(element)) == webdriver.firefox.webelement.FirefoxWebElement
+				element.save_screenshot(p)
+				return 0
+			except Exception as e:
+				print("Unable to save screenshot using given element\n{}".format(e))
+				return -1
+		self.driver.save_screenshot(p)
+		return 0
+
+
 	def getCookies(self):
 		""" Return all cookies """
 		try:
@@ -439,7 +490,32 @@ So the class does not prevent the use of webdriver functions directly, but rathe
 			print("{}".format(e))
 			return -1
 
-				
+
+	def startWindowCheck(self):
+		""" Infinite while loop to be used by thread to check for multiple windows open"""
+		try:
+			assert( type( threading.current_thread() ) ) == threading.Thread
+			print(f"{bcolors.OKCYAN}[+]{bcolors.ENDC}Thread \'%s\' created with PID: %d" % ( threading.current_thread().name, getpid() ))
+		except Exception as e:
+			print(f"{bcolors.FAIL}[!]{bcolors.ENDC}Unable to run startWindowCheck function.\n")
+			print("{}".format(e))
+			return -1
+		try:
+			while True:
+				if self.multipleWindows():
+					print(f"{bcolors.OKGREEN}[+]{bcolors.ENDC} Multiple Windows open.\n")
+					self.handles = self.driver.window_handles
+					for h in self.handles:
+						print("\t[+] Handle : {}".format(h))
+					sleep(30) # Need a better way to wait / wake thread
+				else:
+					sleep(10)
+		except Exception as e:
+			print(f"{bcolors.OKGREEN}[-]{bcolors.ENDC} WindowChecker thread returning.\n")
+			return 1
+		except KeyboardInterrupt:
+			print(f"{bcolors.OKGREEN}[-]{bcolors.ENDC} WindowChecker thread returning.\n")
+			return 2		
 
 	def getChildPIDs(self):
 		""" Update internal pids list of children """
@@ -447,7 +523,7 @@ So the class does not prevent the use of webdriver functions directly, but rathe
 
 	def setChildPIDs(self):
 		""" Loop over running processes and compare to expexted children\
-			if match and parent is python, update internal class self.pids list
+			if match and parent is python, update internal self.pids list
 		"""
 		pids = []
 		for proc in process_iter():
@@ -463,9 +539,12 @@ So the class does not prevent the use of webdriver functions directly, but rathe
 		print(f"{bcolors.OKGREEN}Browser Closing{bcolors.ENDC}...\n")
 		try:
 			self.driver.quit() # Stop the process
+			self.windowChecker.join() # Wait for thread
 		except Exception as e:
 			print(f"{bcolors.WARNING}[!] Unable to Stop process [!]{bcolors.ENDC}")
 			print("{}".format(e))
+			self.windowChecker.join() # Wait for thread
 			exit(-1)
 		print(f"{bcolors.HEADER}[-] Firefox Process Stopped{bcolors.ENDC}")
+		exit(0)
 	## End Browser Control ##
